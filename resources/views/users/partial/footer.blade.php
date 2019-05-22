a30742e170a616df4fecf007596d0e9225c22aca
Loki <rixnickl2@gmail.com>
2019-05-23 01:01:32 +0700

    update initial commit

--- resources/views/users/partial/footer.blade.php
+++ resources/views/users/partial/footer.blade.php
@@ -63,7 +63,7 @@
 		<div class="container">
 			<div class="row">
 
-				<div class="col-lg-3 footer_col">
+				<div class="col-lg-4 footer_col">
 					<div class="footer_column footer_contact">
 						<div class="logo_container">
 							<div class="logo"><a href="#">OneTech</a></div>
@@ -92,9 +92,6 @@
 						<ul class="footer_list">
 							<li><a href="#">Computers & Laptops</a></li>
 							<li><a href="#">Cameras & Photos</a></li>
-							<li><a href="#">Hardware</a></li>
-							<li><a href="#">Smartphones & Tablets</a></li>
-							<li><a href="#">TV & Audio</a></li>
 						</ul>
 						<div class="footer_subtitle">Gadgets</div>
 						<ul class="footer_list">
@@ -102,27 +99,12 @@
 						</ul>
 					</div>
 				</div>
-
-				<div class="col-lg-2">
-					<div class="footer_column">
-						<ul class="footer_list footer_list_2">
-							<li><a href="#">Video Games & Consoles</a></li>
-							<li><a href="#">Accessories</a></li>
-							<li><a href="#">Cameras & Photos</a></li>
-							<li><a href="#">Hardware</a></li>
-							<li><a href="#">Computers & Laptops</a></li>
-						</ul>
-					</div>
-				</div>
-
+				
 				<div class="col-lg-2">
 					<div class="footer_column">
 						<div class="footer_title">Customer Care</div>
 						<ul class="footer_list">
 							<li><a href="#">My Account</a></li>
-							<li><a href="#">Order Tracking</a></li>
-							<li><a href="#">Wish List</a></li>
-							<li><a href="#">Customer Services</a></li>
 							<li><a href="#">Returns / Exchange</a></li>
 							<li><a href="#">FAQs</a></li>
 							<li><a href="#">Product Support</a></li>
@@ -143,7 +125,7 @@
 					
 					<div class="copyright_container d-flex flex-sm-row flex-column align-items-center justify-content-start">
 						<div class="copyright_content"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Brizbox</a>
+Copyright &copy; Power <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Brizbox</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 </div>
 						<div class="logos ml-sm-auto">
